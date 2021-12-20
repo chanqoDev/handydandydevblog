888197fb3d9efd6789b5d008bc237a310366c5b9
Christian E <christianelena92@gmail.com>
2021-12-19 23:00:27 -0600

    add sidebar to setting page and wrapper, title form, icons profile picture

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+
 function App() {
   return (
     <>
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,5 +1,33 @@
 import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function Settings() {
-  return <div className="settings">settings</div>;
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update your account</span>
+          <span className="settingsDeleteTitle">Delete account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img src="./assets/setup.png" alt="" />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Christian" />
+          <label>Email</label>
+          <input type="email" placeholder="christianelena@chriswok.tech" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
 }
