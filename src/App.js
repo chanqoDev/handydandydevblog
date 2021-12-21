5a3da4bc4631b181d29a4fb2607b649266c2196d
Christian E <christianelena92@gmail.com>
2021-12-20 23:05:39 -0600

    add react-router-dom, import BrowserRouter, Routes, Route and specify Routing for components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,34 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Register from "./pages/register/Register";
-function App() {
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  // Pseudo user
+  const currentUser = false;
   return (
-    <>
-      <TopBar />
-      <Register />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/">
+          <Route index element={currentUser ? <Home /> : <Login />} />
+        </Route>
+        <Route path="home" element={currentUser ? <Home /> : <Login />} />
+        <Route path="login" element={currentUser ? <Home /> : <Login />} />
+        <Route
+          path="register"
+          element={currentUser ? <Home /> : <Register />}
+        />
+        <Route path="write" element={currentUser ? <Write /> : <Register />} />
+        <Route
+          path="settings"
+          element={currentUser ? <Settings /> : <Register />}
+        />
+        <Route path="/post/:postId" element={<Single />} />
+        {/* <Route path="*" element={<noPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
