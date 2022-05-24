be671603843413283363442706b43a6c31e73139
Christian E <christianelena92@gmail.com>
2022-05-24 02:29:46 -0500

    SETUP: node js and mongodb connection, env

--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("connected to MongoDB"))
+  .catch((err) => console.error(err));
+
+//inorder for us to run it in the browser
+app.use("/", (req, res) => {
+  console.log("hey this is main url ");
+});
 
 app.listen("5001", () => {
   console.log("backend is running");
